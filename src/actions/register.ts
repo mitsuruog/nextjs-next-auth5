a8c9f78f243dd127c6268238b1e3f5932e7d7ce7
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2024-07-02 21:50:04 +0900

    feat: register function

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,15 +1,37 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 
 import { RegisterFormSchema } from "@/schemes";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterFormSchema>) => {
   const validatedFields = RegisterFormSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid Fields" };
+    return { error: "Invalid Fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: send verification email
+
+  return { success: "User created!" };
 };
